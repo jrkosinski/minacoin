8d6d20087a9dddf96258af1d330c810680fef596
John R. Kosinski <john.kosinski@gmail.com>
2018-05-23 14:17:38 +0700

    moved core to minacoin-core

---
+++ minacoin-core/index.js
@@ -0,0 +1,46 @@
+'use strict'; 
+
+const Chain = require('./classes/Chain'); 
+const Block = require('./classes/Block'); 
+const Wallet = require('./classes/Wallet'); 
+const Transaction = require('./classes/Transaction'); 
+const Output = require('./classes/Output'); 
+
+const chain = new Chain(10); 
+
+//create wallets
+const walletA = new Wallet(chain, 'A'); 
+const walletB = new Wallet(chain, 'B'); 
+const walletC = new Wallet(chain, 'C'); 
+const coinbase = new Wallet(chain, 'coinbase'); 
+
+//genesis transaction, sends 100 to walletA
+const genesisTrans = new Transaction(chain, coinbase.publicKey, coinbase.publicKey, 1000); 
+genesisTrans.generateSignature(coinbase.privateKey); 
+genesisTrans.id= '0'; 
+const output = new Output(genesisTrans.recipient, genesisTrans.amount, genesisTrans.id);
+genesisTrans.outputs.push(output); 
+chain.addUtxo(output); 
+
+//genesis block 
+const genesisBlock = new Block(chain, null); 
+genesisBlock.addTransaction(genesisTrans); 
+genesisBlock.mineBlock(); 
+chain.addBlock(genesisBlock); 
+
+coinbase.print();
+walletA.print();
+walletB.print();
+walletC.print();
+
+//add a new block with another transaction 
+const block1 = new Block(chain, genesisBlock.hash); 
+if (block1.addTransaction(coinbase.sendFunds(walletB.publicKey, 100))) {
+    block1.mineBlock();
+    chain.addBlock(block1); 
+}
+
+coinbase.print();
+walletA.print();
+walletB.print();
+walletC.print();
