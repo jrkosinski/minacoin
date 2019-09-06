a9dc5f8fa841f1d67eea34ab3bf62389a8c1e88c
Acer <john.kosinski@gmail.com>
2019-09-06 21:19:46 +0700

    added babel support for experimental features

---
+++ minacoin-server/src/lib/database/classes/IDatabase.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * minacoin: IDatabase
+ * -------------------
+ * interface for app database which stores things like the blockchain 
+ * and wallet info. 
+ *
+ * author: John R. Kosinski
+ */
+class IDatabase {
+    saveBlockchain(blockchain) {}
+    getBlockchain() {}
+    saveWallet(wallet) {}
+    getWallet() {}
+}
+
+module.exports = IDatabase;
---
+++ minacoin-server/src/util/cryptoUtil.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const LOG_TAG = 'CRPT';
+
+const SHA256 = require('crypto-js/sha256');
+const uuidV1 = require('uuid/v1');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+//TODO: this is far from ideal
+const Signature = require('../../node_modules/elliptic/lib/elliptic/ec/signature');
+
+/**
+ * generates & returns a new elliptic keypair
+ * @returns {KeyPair}
+ */
+/*EC.KeyPair*/ exports.generateKeyPair = () => {
+    return ec.genKeyPair();
+};
+
+/**
+ * deserializes a key pair from string (hex) representations of its 
+ * public & private keys
+ * @param pub string (hex) representation of public key
+ * @param priv string (hex) representation of private key
+ * @returns {KeyPair}
+ */
+/*EC.KeyPair*/ exports.deserializeKeyPair = (pub, priv) => {
+    return ec.keyPair({
+        priv: priv,
+        privEnc: 'hex',
+        pub: pub,
+        pubEnc: 'hex'
+    });
+};
+
+/**
+ * generates & returns a new UUID
+ * @returns {string}
+ */
+/*string*/ exports.id = () => {
+    return uuidV1();
+};
+
+/**
+ * generates a SHA256 hash of given data 
+ * @param {json} data 
+ */
+/*string*/ exports.hash = (data) => {
+    return SHA256(JSON.stringify(data)).toString();
+}
+
+/**
+ * validates a signature 
+ * @param {string} publicKey
+ * @param {string} signature
+ * @param {string} dataHash
+ * @returns {bool}
+ */
+/*bool*/ exports.verifySignature = (publicKey, signature, dataHash) => {
+    return ec.keyFromPublic(publicKey,'hex').verify(dataHash, signature);
+}
+
+exports.serializeSignature = (signature) => {
+    return {
+        r: signature.r.toString('hex'), 
+        s: signature.s.toString('hex')
+    };
+}
+
+exports.deserializeSignature = (json) => {
+    return new Signature(json, 'hex'); 
+}
