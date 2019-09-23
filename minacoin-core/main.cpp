f501766df901726162b28f7017b386db5e1000c5
Acer <john.kosinski@gmail.com>
2019-09-23 23:31:54 +0700

    removed test code from main

--- minacoin-core/main.cpp
+++ minacoin-core/main.cpp
@@ -37,68 +37,6 @@ IOC* initializeIoc() {
 
 int main() {
 	initializeIoc(); 
-	
-	auto logger = IOC::resolve<ILoggerFactory>()->createLogger("MAIN");
-	
-	
-        auto server1 = make_unique<Server>(false); //sender
-        auto server2 = make_unique<Server>(false); //receiver
-        auto senderWallet = server1->wallet(); 
-        auto receiverWallet = server2->wallet(); 
-        
-        float senderBal1 = senderWallet->balance();
-        float recipBal1 = receiverWallet->balance();
-        float txAmount = 100;
-        
-        senderWallet->send(receiverWallet->address(), txAmount, server1->blockchain(), server1->txPool()); 
-        server1->miner()->mine(); 
-        
-        server2->blockchain()->replaceChain(server1->blockchain()); 
-        
-        /*
-        senderWallet->updateBalance(server1->blockchain()); 
-        receiverWallet->updateBalance(server2->blockchain()); 
-        
-        float senderBal2 = senderWallet->balance();
-        float recipBal2 = receiverWallet->balance();
-        
-        REQUIRE(senderBal2 < senderBal1); 
-        REQUIRE(recipBal1 > recipBal2); 
-        REQUIRE(recipBal2 == (recipBal1 + txAmount));
-		*/
-	
-	/*	
-	auto server = make_unique<Server>(false); 
-	
-	//Blockchain* blockchain = new Blockchain(); 
-	auto blockchain = server->blockchain(); 
-	auto txPool = server->txPool(); 
-	auto wallet = server->wallet();
-	auto miner = server->miner();
-	
-	//string compressed = minacoin::util::base64::string_compress_encode(wallet->address()); 
-	//logger->info(compressed); 
-	
-	logger->info("blockchain height is %d", (int)blockchain->height()); 
-	logger->info("genesis block hash is %s", blockchain->blockAt(0)->hash().c_str());
-	
-	Transaction* trans1 = wallet->send("48948948948", 100, blockchain, txPool);
-	Transaction* trans2 = wallet->send("4894894e948", 100, blockchain, txPool);
-	Transaction* trans3 = wallet->send("489489489dd", 100, blockchain, txPool);
-	
-	Block* newBlock = miner->mine();
-	blockchain->lastBlock()->fromJson("{\"data\":[],\"difficulty\":3,\"hash\":\"12121\",\"lastHash\":\"---\",\"nonce\":0,\"timestamp\":0}"); 
-
-	logger->info(blockchain->lastBlock()->hash()); 
-	logger->info(blockchain->isValid() ? "VALID" : "NOT VALID"); 
-	
-	auto db = IOC::resolve<IDatabase>();
-	db->saveBlockchain(blockchain); 
-	db->saveWallet(wallet); 
-	
-	Blockchain* bc2 = db->getBlockchain(); 
-	Wallet* w2 = db->getWallet(); 
-	*/
 
 	return 0;
 }
